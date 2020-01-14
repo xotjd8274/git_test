3873fc684ec34e23303f19dd89d139180d7516cb
taeseong <xotjd4616@naver.com>
2020-01-14 15:30:05 +0900

    fourth commit in branch

--- TestClass.java
+++ TestClass.java
@@ -1,7 +1,7 @@
 
 public class TestClass{
     public static void main (String[] args){
-        System.out.println("Hi!");
+        System.out.println("Hi!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         System.out.println("It's too cold!");
     }
 }
